f01ba4f8b8391b84d1d9496690c62053920b5b47
Luis Miguel Serrano <luis.miguel.santos.serrano@gmail.com>
2017-08-26 20:59:31 +0100

    Fixing things: - Changing API endpoints in use - Re-added support for summoner names with spaces

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var https         = require('https');
 
 // Load environment variables
 if (process.env.NODE_ENV === 'development') {
-  require('dotenv').config();
+    require('dotenv').config();
 }
-var api_key = process.env.LOL_API_KEY;
-var port    = process.env.PORT;
+//var api_key = process.env.LOL_API_KEY;
+//var port    = process.env.PORT;
+// NOTE: TEMP DEBUG - Don't commit and push this
+var api_key = '???';
+var port = '8080';
 
 // Defined values
 var rank  = ['S+', 'S', 'S-', 'A+', 'A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'No Rank'];
@@ -22,218 +25,219 @@ var DEBUG = false;
 // Main page
 app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname+ '/public/index.html'));
+    res.sendFile(path.join(__dirname+ '/public/index.html'));
 });
 
 // API queries
 app.post('/info/:region/:username/:role*?', function (req, res) {
-  // res.header('Access-Control-Allow-Origin', '*');
-  if (req.params.role == undefined) {
-    req.params.role = 'all';
-  }
-  console.log('Info request for ' + req.params.username + ' in ' + req.params.region + ' for role ' + req.params.role);
-  getSummonerId(req.params, res);
+    // res.header('Access-Control-Allow-Origin', '*');
+    if (req.params.role == undefined) {
+        req.params.role = 'all';
+    }
+    console.log('Info request for ' + req.params.username + ' in ' + req.params.region + ' for role ' + req.params.role);
+    getSummonerId(req.params, res);
 })
 
 
 app.listen(port, function () {
-  console.log('App started at localhost:' + port);
+    console.log('App started at localhost:' + port);
 });
 
 /**
-Preforms a GET request to the url specified and calls success or error with the result
-**/
+ Preforms a GET request to the url specified and calls success or error with the result
+ **/
 function makeRequest(url, success, error) {
-  // console.log(url);
-  https.get(url,
-    (response) => {
-      // console.log('statusCode: ', response.statusCode);
-      // console.log('headers: ', res.headers);
-      if (response.statusCode != 200) {
+    https.get(url,
+        (response) => {
+        // console.log('statusCode: ', response.statusCode);
+        // console.log('headers: ', res.headers);
+        if (response.statusCode != 200) {
         console.log('Error: ' + response.statusCode);
         error({"error" : "status code " + response.statusCode});
         return;
-      }
+    }
 
-      var data = '';
+    var data = '';
 
-      response.on('data', (d) => {
+    response.on('data', (d) => {
         data += d;
-      });
+});
 
-      response.on('end', () => {
+    response.on('end', () => {
         success(data);
-      });
+});
 
-    }).on('error', (e) => {
-      console.log('Error: ' + e);
-      error({"error" : e});
-    });
+}).on('error', (e) => {
+        console.log('Error: ' + e);
+    error({"error" : e});
+});
 }
 
 
 /****** Preform additional quries to assemble information ******/
 
 function getSummonerId(params, res) {
-  var summoner_name = params.username;
-  var region = params.region;
-  var url = 'https://'+region+'.api.pvp.net/api/lol/'+region+'/v1.4/summoner/by-name/'+summoner_name+'?api_key='+api_key;
-  makeRequest(url,
-    (data) => {
+    var summoner_name = encodeURIComponent(params.username);
+    var region = regions[params.region].toLowerCase();
+    var url = 'https://'+region+'.api.riotgames.com/lol/summoner/v3/summoners/by-name/'+summoner_name+'?api_key='+api_key;
+    console.log('Calling url: ' + url);
+    makeRequest(url,
+        (data) => {
         data = JSON.parse(data);
-        for (var user in data) {
-          getChampionMastery({
+        getChampionMastery({
             params: params,
-            summoner_id: data[user].id,
-            summoner_name: data[user].name,
-            }, res);
-          break;
-        }
+            summoner_id: data.id,
+            summoner_name: data.name,
+        }, res);
     },
     (error) => {
-      res.json(error);
+        console.log("Error on getSummonerId for params: " + JSON.stringify(params));
+        res.json(error);
     });
 }
 
 function getChampionMastery(data, res) {
-  var region = data.params.region;
-  var url = 'https://'+region+'.api.pvp.net/championmastery/location/'+regions[region]+'/player/'+data['summoner_id']+'/champions?api_key='+api_key;
+    var region = regions[data.params.region].toLowerCase();
+    var url = 'https://'+region+'.api.riotgames.com/lol/champion-mastery/v3/champion-masteries/by-summoner/'+data['summoner_id']+'?api_key='+api_key;
     makeRequest(url,
-    (mastery_info) => {
+        (mastery_info) => {
         mastery_info = JSON.parse(mastery_info);
-        addChampionInfo({
-          params: data.params,
-          summoner_id: data['summoner_id'],
-          summoner_name: data['summoner_name'],
-          champions: mastery_info,
-          mastery_info: mastery_info,
-        }, res);
-    },
+    addChampionInfo({
+        params: data.params,
+        summoner_id: data['summoner_id'],
+        summoner_name: data['summoner_name'],
+        champions: mastery_info,
+        mastery_info: mastery_info,
+    }, res);
+},
     (error) => {
-      res.json(error);
+        console.log("Error on getChampionMastery for data: " + JSON.stringify(data));
+        res.json(error);
     });
 }
 
 function addChampionInfo(data, res) {
-  var region = data.params.region.toLowerCase();
-  url = 'https://global.api.pvp.net/api/lol/static-data/'+region+'/v1.2/champion?dataById=false&champData=image&api_key='+api_key;
-  makeRequest(url,
-    (champlist) => {
+    var region = regions[data.params.region].toLowerCase();
+    var url = 'https://global.api.pvp.net/api/lol/static-data/'+region+'/v1.2/champion?dataById=false&champData=image&api_key='+api_key;
+    makeRequest(url,
+        (champlist) => {
         champlist = JSON.parse(champlist).data;
-        var champArray = [];
-        for (var championInfo in champlist) {
-          champArray.push(combineChampionInfo(champlist[championInfo],data.mastery_info));
-        }
-        data.champions = champArray;
-        data.champions.sort(compareChampion);
-        getVersion(data, res);
-    },
+    var champArray = [];
+    for (var championInfo in champlist) {
+        champArray.push(combineChampionInfo(champlist[championInfo],data.mastery_info));
+    }
+    data.champions = champArray;
+    data.champions.sort(compareChampion);
+    getVersion(data, res);
+},
     (error) => {
-      res.json(error);
+        console.log("Error on addChampionInfo for data: " + JSON.stringify(data));
+        res.json(error);
     });
 }
 
 function getVersion(data, res) {
-  var region = data.params.region;
-  var url = 'https://global.api.pvp.net/api/lol/static-data/'+region+'/v1.2/versions?api_key='+api_key;
+    var region = regions[data.params.region].toLowerCase();
+    var url = 'https://global.api.pvp.net/api/lol/static-data/'+region+'/v1.2/versions?api_key='+api_key;
 
-  makeRequest(url,
-    (versions) => {
+    makeRequest(url,
+        (versions) => {
         versions = JSON.parse(versions);
-        data.version = versions[0];
-        filterChamptions(data, res);
-    },
+    data.version = versions[0];
+    filterChamptions(data, res);
+},
     (error) => {
-      res.json(error);
+        console.log("Error on getVersion for data: " + JSON.stringify(data));
+        res.json(error);
     });
 }
 
 function filterChamptions(data, res) {
-  data.has_chest = data.champions.filter(championHasChest);
-  data.recommended = data.champions.filter(championHasNoChest).filter(function(c) {return championRoleMatches(c, data.params.role);}).slice(0,NUMBER_CHAMPS_TO_RECOMMEND);
-  displayData(data, res);
+    data.has_chest = data.champions.filter(championHasChest);
+    data.recommended = data.champions.filter(championHasNoChest).filter(function(c) {return championRoleMatches(c, data.params.role);}).slice(0,NUMBER_CHAMPS_TO_RECOMMEND);
+    displayData(data, res);
 }
 
 
 /**
-Responds with the assembled information
-**/
+ Responds with the assembled information
+ **/
 function displayData(data, res) {
-  if (DEBUG) {
-    data.has_chest = data.has_chest.map(function (c) {return [c.name, c.highest_grade, c.champion_points]});
-    data.recommended = data.recommended.map(function (c) {return [c.name, c.highest_grade, c.champion_points]});
-  }
-  res.json({
-    "summoner_name":  data.summoner_name,
-    "summoner_id":    data.summoner_id,
-    "has_chest":      data.has_chest,
-    "recommended":    data.recommended,
-    "version":        data.version,
-  })
+    if (DEBUG) {
+        data.has_chest = data.has_chest.map(function (c) {return [c.name, c.highest_grade, c.champion_points]});
+        data.recommended = data.recommended.map(function (c) {return [c.name, c.highest_grade, c.champion_points]});
+    }
+    res.json({
+        "summoner_name":  data.summoner_name,
+        "summoner_id":    data.summoner_id,
+        "has_chest":      data.has_chest,
+        "recommended":    data.recommended,
+        "version":        data.version,
+    })
 
 }
 
 /****** Helper Functions ******/
 
 function combineChampionInfo(championInfo, championMasteryList) {
-  var championMastery = championMasteryList.find(function(c) {
-    return c.championId == championInfo.id;
-  });
+    var championMastery = championMasteryList.find(function(c) {
+        return c.championId == championInfo.id;
+    });
 
-  var result = {
-      'name'          : championInfo.name,
-      'title'         : championInfo.title,
-      'key'           : championInfo.key,
-      'highest_grade' : "No Rank",
-      'champion_points': 0,
-      'image'         : championInfo.image,
-      'has_chest'     : false,
-      'id'            : championInfo.id
+    var result = {
+        'name'          : championInfo.name,
+        'title'         : championInfo.title,
+        'key'           : championInfo.key,
+        'highest_grade' : "No Rank",
+        'champion_points': 0,
+        'image'         : championInfo.image,
+        'has_chest'     : false,
+        'id'            : championInfo.id
     };
 
-  if (championMastery != undefined) {
-    if (championMastery.highestGrade !== undefined) {
-      result.highest_grade    = championMastery.highestGrade;
+    if (championMastery != undefined) {
+        if (championMastery.highestGrade !== undefined) {
+            result.highest_grade    = championMastery.highestGrade;
+        }
+        if (championMastery.championPoints != undefined) {
+            result.champion_points  = championMastery.championPoints;
+        }
+        if (championMastery.chestGranted != undefined) {
+            result.has_chest        = championMastery.chestGranted;
+        }
     }
-    if (championMastery.championPoints != undefined) {
-      result.champion_points  = championMastery.championPoints;
-    }
-    if (championMastery.chestGranted != undefined) {
-      result.has_chest        = championMastery.chestGranted;
-    }
-  }
-  return result;
+    return result;
 }
 
 function championHasChest(championMastery) {
-  return championMastery.has_chest;
+    return championMastery.has_chest;
 }
 function championHasNoChest(championMastery) {
-  return !championHasChest(championMastery);
+    return !championHasChest(championMastery);
 }
 function championRoleMatches(champion, role) {
-  return role == 'all' || (role in championRoles && championRoles[role].indexOf(champion.key) != -1);
+    return role == 'all' || (role in championRoles && championRoles[role].indexOf(champion.key) != -1);
 }
 
 function compareChampion(c1, c2) {
-  // return c2.champion_points - c1.champion_points;
-  var score1 = c1.highest_grade;
-  var score2 = c2.highest_grade;
-  if (score1 == null && score2 == null || rank.indexOf(score1) == rank.indexOf(score2)) {
-    // Compare mastery points
-    if (c1.champion_points == c2.champion_points) {
-      // Compare win rates
-      return 0;
+    // return c2.champion_points - c1.champion_points;
+    var score1 = c1.highest_grade;
+    var score2 = c2.highest_grade;
+    if (score1 == null && score2 == null || rank.indexOf(score1) == rank.indexOf(score2)) {
+        // Compare mastery points
+        if (c1.champion_points == c2.champion_points) {
+            // Compare win rates
+            return 0;
+        } else {
+            return c2.champion_points - c1.champion_points;
+        }
+    } else if (score2 == null) {
+        return -1;
+    } else if (score1 == null) {
+        return 1;
     } else {
-      return c2.champion_points - c1.champion_points;
+        return rank.indexOf(score1) - rank.indexOf(score2);
     }
-  } else if (score2 == null) {
-    return -1;
-  } else if (score1 == null) {
-    return 1;
-  } else {
-    return rank.indexOf(score1) - rank.indexOf(score2);
-  }
 }
 
 /*
--- public/js/script.js
+++ public/js/script.js
@@ -1,119 +1,119 @@
 
 var querying = false; 	// Prevents users spamming the search button
 function loadPlayer() {
-	if (querying) return;
+    if (querying) return;
 
-	clearPage();
+    clearPage();
 
-	var region = $('#regionSelect').val();
-	var username = $('#summonerName').val();
-	username = encodeURIComponent(username.replace(/ /g, ''));
-	var role = $('#roleSelect').val();
-	var url = '/info/'+region+'/'+username+'/'+role;
-	// $.get(url,
-	$.ajax({
-		type: "post",
-		url: url,
-	    beforeSend: function() {
-	    	querying = true;
-	    	$('#submitButton').addClass('m-progress');
-	    },
-		success: function (data) {
-			// querying = false;
-			if (data.error != undefined) {
-				showError(clarifyError(data.error));
-				return;
-			}
+    var region = $('#regionSelect').val();
+    var username = $('#summonerName').val();
+    username = encodeURIComponent(username); //.replace(/ /g, ''));
+    var role = $('#roleSelect').val();
+    var url = '/info/'+region+'/'+username+'/'+role;
+    // $.get(url,
+    $.ajax({
+        type: "post",
+        url: url,
+        beforeSend: function() {
+            querying = true;
+            $('#submitButton').addClass('m-progress');
+        },
+        success: function (data) {
+            // querying = false;
+            if (data.error != undefined) {
+                showError(clarifyError(data.error));
+                return;
+            }
 
-			$('#formError').addClass('hidden');
+            $('#formError').addClass('hidden');
 
-			var showRec = (data.recommended.length > 0);
-			var showOwned = (data.has_chest.length > 0);
+            var showRec = (data.recommended.length > 0);
+            var showOwned = (data.has_chest.length > 0);
 
-			if (showRec) {
-				$("#recChampsContainer").html(displayChamps(data.recommended, true, data.version));
-				$('#recChamps').fadeIn('slow');
-				$('#recChamps').removeClass('hidden');
-			}
-			if (showOwned) {
-				$("#ownedChampsContainer").html(displayChamps(data.has_chest, false, data.version));
-				$('#ownedChamps').fadeIn(800);
-				$('#ownedChamps').removeClass('hidden');
-			}
-			if (!showRec && !showOwned) {
-				showError('No information was found. Sorry!');
-			}
+            if (showRec) {
+                $("#recChampsContainer").html(displayChamps(data.recommended, true, data.version));
+                $('#recChamps').fadeIn('slow');
+                $('#recChamps').removeClass('hidden');
+            }
+            if (showOwned) {
+                $("#ownedChampsContainer").html(displayChamps(data.has_chest, false, data.version));
+                $('#ownedChamps').fadeIn(800);
+                $('#ownedChamps').removeClass('hidden');
+            }
+            if (!showRec && !showOwned) {
+                showError('No information was found. Sorry!');
+            }
 
-			$('html,body').animate({
-			   scrollTop: $(".results").offset().top
-			});
-		},
-	    error: function (jqXHR, textStatus, errorThrown) {
-	    	showError(clarifyError(jqXHR.statusCode().status+': '+jqXHR.responseText));
-	    },
-	    complete: function() {
-	    	querying = false;
-	    	$('#submitButton').removeClass('m-progress');
-	    }
-	});
+            $('html,body').animate({
+                scrollTop: $(".results").offset().top
+            });
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            showError(clarifyError(jqXHR.statusCode().status+': '+jqXHR.responseText));
+        },
+        complete: function() {
+            querying = false;
+            $('#submitButton').removeClass('m-progress');
+        }
+    });
 }
 
 function showError(error) {
-	$('#formError').html('An error has occured. ' + error);
-	$('#formError').removeClass('hidden');
+    $('#formError').html('An error has occured. ' + error);
+    $('#formError').removeClass('hidden');
 }
 
 function clarifyError(error) {
-	console.log(error);
-	if (error.indexOf('404') !== -1) {
-		return 'Summoner not found!';
-	} else if (error.indexOf('500') !== -1) {
-		return 'Something bad happened!';
-	} else if (error.indexOf('429') !== -1) {
-		return 'Please try again later.';
-	}
-	return "Sorry!";
+    console.log(error);
+    if (error.indexOf('404') !== -1) {
+        return 'Summoner not found!';
+    } else if (error.indexOf('500') !== -1) {
+        return 'Something bad happened!';
+    } else if (error.indexOf('429') !== -1) {
+        return 'Please try again later.';
+    }
+    return "Sorry!";
 }
 
 function displaySplash(champ) {
-	var url = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champ.key+"_0.jpg";
-	var champHtml = "<img src=\""+url+"\">"
-		+ "<div><span class=\"championName\">"+champ.name+"</span><br>"
-		+ "Mastery Points: "+champ.champion_points+"</span>"
-		+"</div>";
-	$('.championSplash').html(champHtml);
+    var url = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + champ.key+"_0.jpg";
+    var champHtml = "<img src=\""+url+"\">"
+        + "<div><span class=\"championName\">"+champ.name+"</span><br>"
+        + "Mastery Points: "+champ.champion_points+"</span>"
+        +"</div>";
+    $('.championSplash').html(champHtml);
 }
 
 function displayChamps(json, showSplash, version) {
-	champs = json;
-	if (champs.length == 0) {
-		return "<div>We couldn't find any data. Sorry!</div>";
-	}
+    champs = json;
+    if (champs.length == 0) {
+        return "<div>We couldn't find any data. Sorry!</div>";
+    }
 
-	var champHtml = "";
-	for(var i = 0; i < champs.length; i++) {
-		if (showSplash && i==0) {
-			displaySplash(champs[i]);
-		} else {
-		champHtml += "<div class=\"championPortrait\" title=\"" + champs[i].name + "\">"
-			+ "<img src=\"http://ddragon.leagueoflegends.com/cdn/" + version + "/img/champion/" + champs[i].image.full + "\"/>"
-			+ "<span class=\"championGrade\">"+champs[i].champion_points + "</span>"
-			+ (showSplash?"<span class=\"championName\">"+champs[i].name+"</span>":"")
-			+ "</div>";
-		}
-	}
-	return champHtml;
+    var champHtml = "";
+    for(var i = 0; i < champs.length; i++) {
+        if (showSplash && i==0) {
+            displaySplash(champs[i]);
+        } else {
+            champHtml += "<div class=\"championPortrait\" title=\"" + champs[i].name + "\">"
+                + "<img src=\"http://ddragon.leagueoflegends.com/cdn/" + version + "/img/champion/" + champs[i].image.full + "\"/>"
+                + "<span class=\"championGrade\">"+champs[i].champion_points + "</span>"
+                + (showSplash?"<span class=\"championName\">"+champs[i].name+"</span>":"")
+                + "</div>";
+        }
+    }
+    return champHtml;
 }
 
 function clearPage() {
-	var blank = '';
+    var blank = '';
 
-	$('#formError').addClass('hidden');
+    $('#formError').addClass('hidden');
 
-	$("#ownedChampsContainer").html(blank);
-	$("#recChampsContainer").html(blank);
-	$('.championSplash').html(blank);
-	$('#ownedChamps').addClass('hidden');
-	$('#recChamps').addClass('hidden');
+    $("#ownedChampsContainer").html(blank);
+    $("#recChampsContainer").html(blank);
+    $('.championSplash').html(blank);
+    $('#ownedChamps').addClass('hidden');
+    $('#recChamps').addClass('hidden');
 
 }
